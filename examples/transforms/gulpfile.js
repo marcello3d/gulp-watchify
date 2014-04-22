c75a46a81e685265ed546d13d871470a2b69e7e6
Marcello Bastea-Forte <marcello@cellosoft.com>
2014-04-22 10:31:28 -0700

    transforms example via setup option

---
+++ examples/transforms/gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp')
+
+var watchify = require('../..')
+
+var bundlePaths = {
+    src: [
+        'client/js/**/*.js',
+        "!client/js/**/lib/**" // Don't bundle libs
+    ],
+    dest:'build/js/'
+}
+
+
+// Hack to enable configurable watchify watching
+var watching = false
+gulp.task('enable-watch-mode', function() { watching = true })
+
+// Browserify and copy js files
+gulp.task('browserify', watchify(function(watchify) {
+    return gulp.src(bundlePaths.src)
+        .pipe(watchify({
+            watch:watching,
+            setup: function(bundle) {
+                bundle.transform(require('brfs'))
+            }
+        }))
+        .pipe(gulp.dest(bundlePaths.dest))
+}))
+
+gulp.task('watchify', ['enable-watch-mode', 'browserify'])
+
+
+// Rerun tasks when a file changes
+gulp.task('watch', ['watchify'], function () {
+    // ... other watch code ...
+})
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['browserify'])
