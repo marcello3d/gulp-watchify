f64d54e7da9f9ab8c1593c04765595fdb12f221c
Marcello Bastea-Forte <marcello@cellosoft.com>
2015-03-18 08:13:48 -0700

    rewrite tests to not use exec-sync
    - exec-sync uses native code and is incompatible with io.js
    - io.js actually has its own execSync method, but for this test it’s fine just to run async

--- test.js
+++ test.js
@@ -1,21 +1,23 @@
 var fs = require('fs');
-var sys = require('sys');
-var exec = require('exec-sync');
-var child;
+var child_process = require('child_process');
 var path = require('path');
 
 var examples = fs.readdirSync('./examples').map(function(file){
-  return path.join('./examples', file);
+    return path.join('./examples', file);
 }).filter(function (file) {
-  return fs.statSync(file).isDirectory();
+    return fs.statSync(file).isDirectory();
 });
 
 examples.forEach(function(dir) {
-  var pwd = process.cwd();
-  process.chdir(dir);
+    var pwd = process.cwd();
 
-  console.log('Running: ' + dir);
-  exec('gulp');
-
-  process.chdir(pwd);
+    console.log('Running async gulp child process in ' + dir);
+    child_process.exec('gulp', { cwd: dir }, function(err, result) {
+        if (err) {
+            console.error(dir+' failed')
+            throw err
+        } else {
+            console.log(dir+'\n'+result)
+        }
+    });
 });
